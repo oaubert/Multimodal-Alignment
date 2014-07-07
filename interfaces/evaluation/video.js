4d6ff0ca87f73b19f221233f51b6659f0ddf0017
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2014-07-07 17:46:16 +0200

    Comments

--- interfaces/evaluation/video.js
+++ interfaces/evaluation/video.js
@@ -1,6 +1,17 @@
-var glob_deb;
-var glob_fin;
+/*** Fonction permettant d'afficher et de contrôler la vidéo ***/
 
+/** Permet de passer seulement un passage donné de la vidéo **/
+
+var glob_deb; //Variable globale conservant le début du passage
+var glob_fin; //Variable globale conservant la fin du passage
+
+
+/* Initialise la vidéo
+	-Initialise les variables globales
+	-Place le temps courant de la vidéo au temps de début
+	-Met un listener sur l'update de temps pour vérifier à chaque défilement de la vidéo si le passage est fini : appelle la fonction fini
+	-Initialise le bouton play. La vidéo est actuellement en pause
+*/
 function initialisation(debut, fin)
 {
 	glob_deb = debut;
@@ -13,6 +24,10 @@ function initialisation(debut, fin)
 	buttonPlay = document.getElementById("play");
 }
 
+/* Vérifie si le passage est fini
+	-Test si le temps courant est le temps de fin du passage
+	-Si c'est le cas, met la vidéo en pause et désactive le bouton play (le bouton replay est toujours disponible)
+*/
 function fini()
 {
 	if(this.currentTime >= glob_fin)
@@ -22,6 +37,7 @@ function fini()
 	}
 }
 
+/* Passe de pause à play ou inversement */
 function playPause()
 {
 	if(video.paused)
@@ -35,7 +51,8 @@ function playPause()
 		buttonPlay.innerHTML = "Play";
 	}
 }
-	
+
+/* Relance le passage de la vidéo au début */
 function replay()
 {
 	video.currentTime = glob_deb;
