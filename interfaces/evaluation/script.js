4f3eee5068ec84809b44031c9ec7aedc610f2c1c
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2014-07-07 18:00:42 +0200

    Comments

--- interfaces/evaluation/script.js
+++ interfaces/evaluation/script.js
@@ -1,8 +1,21 @@
-var dataSpeech;
-var dataLink;
-var res = {};
-var idSpeech = 0;
+/*** Gère l'affichage de l'interface d'évaluation ***/
 
+/** Cette interface affiche speech par speech les trois meilleurs paragraphes correspondant.
+	On sélectionne ensuite pour chaque paragraphe si il correspond ou pas, avant de passer au speech suivant.
+
+	Cela permet une première évaluation des résultats.
+	Elle ne prend cependant pas en compte le fait que les paragraphes peuvent correspondre au speech, sans être les meilleurs résultats possibles.
+**/
+
+var dataSpeech; 	//Variable globale contenant les données sur le speech actuel
+var dataLink;		//Variable globale contenant les données sur les liens correspondant au speech actuel
+var res = {};		//Variable globale contenant le résultat de l'évaluation
+var idSpeech = 0;	//Variable globale contenant l'id du speech actuel
+
+/* Affiche les informations du n-ème meilleur paragraphe
+	-La similarité dans la baliseSimilarite
+	-Le texte du paragraphe dans la baliseTexte
+*/
 function afficherParagraphe(n, baliseSimilarite, baliseTexte)
 {
 	var link = dataLink[n];
@@ -14,6 +27,13 @@ function afficherParagraphe(n, baliseSimilarite, baliseTexte)
 }
 
 
+/* Affiche l'interface pour le slide actuel
+	-Vérifie que le slide est correct, sinon redirige vers la page fini.php
+	-Vérifie que les radio button ne sont pas déjà coché
+	-Sélectionne les données du speech actuel et des liens correspondants
+	-Affiche les trois meilleurs paragraphes
+	-Initialise la vidéo
+*/
 function afficher()
 {
 	if(idSpeech >= nbSpeech)
@@ -36,7 +56,7 @@ function afficher()
 	initialisation(debut, fin);
 }
 
-
+/* Enregistre l'évaluation du speech actuel et passe au suivant */
 function resultat()
 {
 	res[idSpeech] = {"1" : document.getElementById('p1_Oui').checked, "2" : document.getElementById('p2_Oui').checked, "3" : document.getElementById('p3_Oui').checked};
@@ -46,6 +66,7 @@ function resultat()
 	afficher();
 }
 
+/* Remet à zéro l'évaluation */
 function resetEvaluation()
 {
 	res = {};
